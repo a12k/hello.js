0d589c7aeb8e702b7c4b066fe80c2956918dfbbe
a12k <aaron.wieczorek@gmail.com>
2016-11-30 09:52:57 -0600

    scaffolding

--- src/modules/strava.js
+++ src/modules/strava.js
@@ -1,1 +1,24 @@
+(function(hello) {
 
+	hello.init({
+		
+		strava: {
+			name: 'Strava',
+			
+			// SEE https://strava.github.io/api/v3/oauth/
+			oauth: {
+				version: 2,
+				auth: 'https://www.strava.com/oauth/authorize',
+				grant: 'https://www.strava.com/oauth/token'
+			},
+			
+			// Authorization scopes
+			scope: {
+				'',
+				'view_private',
+				'write'
+			},
+		}
+	});
+	
+})(hello);
